8fd63dd17a0407e968dd955eaf1aa8f2138f2deb
NotBen <benramirez805@gmail.com>
2024-07-03 21:21:05 -0700

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 def main():
     email = input('Enter your email: ')
-    """
+    
     ########################################
-    Code Your Program here
+    if email[0].isalpha():
+        print("True")
+    else:
+        print("False")
+    
+    if 5 < len(email) < 30:
+        print("True")
+    else:
+        print("False")
+    
+    if email.find('@') == -1 or email.find('. after @') == -1:
+        print("False")
+    else:
+        print("True")
+
+    
+
     ########################################
-    """
+    
     result = True
 
     ########################################
